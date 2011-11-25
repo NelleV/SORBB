025c60e53d8829f03579c192710a3ad66e033b22
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2011-11-25 21:16:47 +0100

    Now computes the boundary descriptors of an image

--- src/descriptors.py
+++ src/descriptors.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from sklearn.metrics.pairwise import euclidean_distances
+from skimage.feature.hog import hog
 
 def get_interest_points(calc, min_dist=40):
     """
@@ -15,6 +16,9 @@ def get_interest_points(calc, min_dist=40):
 
     returns
     -------
+        ndarray:
+            array of shape (n, 2) containing the indexes of the interest
+            points.
 
     References
     -----------
@@ -29,24 +33,108 @@ def get_interest_points(calc, min_dist=40):
     for i in range(0, delta.shape[0]):
         for j in range(0, delta.shape[1]):
             if delta[i, j]:
-                point = [j, i]
+                point = [i, j]
                 if not interest_points or \
                    euclidean_distances(point, interest_points).min() > min_dist:
-                    interest_points.append([j, i])
+                    interest_points.append([i, j])
 
     return np.array(interest_points)
+
+
+def get_patch(image, mask, points):
+    """
+    Creates the patches for each interest points
+
+    params
+    ------
+       image: ndarray of shape (., ., 3)
+
+       points: ndarray of shape (N, 2)
+
+    returns
+    --------
+
+    """
+    im = image.mean(axis=2)
+    for point in points:
+        # Let's first start by creating the patches
+        point = points[1, :]
+        patch = im[point[0] - 16:point[0] + 16, point[1] - 16:point[1] + 16]
+        mask_patch = mask[point[0] - 16:point[0] + 16, point[1] - 16:point[1] + 16]
+        yield patch, mask_patch
+
+
+def occupancy_grid(patch):
+    """
+    Computes the occupancy grid as descrive in [1]_
+
+    params
+    ------
+        patch: ndarray of shape 16, 16
+
+    returns
+    --------
+        feature: list of 4 elements
+
+    references
+    ----------
+    [1]_ Smooth Object Retrieval using a Bag of Boundaries
+    """
+    feature = []
+    feature.append(patch[:16, :16].sum() / 256)
+    feature.append(patch[:16, 16:].sum() / 256)
+    feature.append(patch[16:, :16].sum() / 256)
+    feature.append(patch[:16, 16:].sum() / 256)
+
+    return feature
+
+
+
+def compute_boundary_desc(image, mask, points):
+    """
+    Compute the boundary descriptors as described in [1]_
+
+    params
+    ------
+       image: ndarray of shape (., ., 3)
+
+       mask: ndarray of shape (., .)
+
+       points: ndarray of shape (N, 2)
+
+    returns
+    -------
+       features: list of arrays
+    """
+
+    gen = get_patch(image, mask, points)
+
+    features = []
+    for patch, mask_patch in gen:
+        feature = []
+        feature.append(hog(patch))
+        feature.append(occupancy_grid(patch))
+        features.append(np.array(feature).flatten())
+
+    return features
+
+
 
 
 if __name__ == "__main__":
     import load
     import matplotlib.pyplot as plt
+
+    from sklearn.externals.joblib import Memory
+    mem = Memory(cachedir='.')
+
     gen = load.load_data()
     _, _ = gen.next()
     im, mask = gen.next()
-    points = get_interest_points(mask, min_dist=40)
+    points = mem.cache(get_interest_points)(mask, min_dist=40)
 
-    plt.figure()
-    plt.imshow(mask)
-    plt.scatter(points[:, 0], points[:, 1])
+    features = compute_boundary_desc(im, mask, points)
+
+
 
 
