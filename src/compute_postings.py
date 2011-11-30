7545660aec8f75260a427f044e52dbd2e930bbaa
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2011-11-30 19:14:40 +0100

    Added an option in the scripts to compute vocabulary and postings to specify the number of images

--- src/compute_postings.py
+++ src/compute_postings.py
@@ -6,18 +6,21 @@ from load import load_data
 from descriptors import compute_boundary_desc, get_interest_points
 from histograms import compute_visual_words
 
+NUM_IMAGES = 50
+
 mem = Memory(cachedir='.')
 
 vocabulary = np.load('./data/vocabulary.mat')
 gen = load_data()
 res = []
 
+# FIXME needs to lookup the number of images
 postings = np.zeros((len(vocabulary), 3170))
 
 for i, (im, mask) in enumerate(gen):
     if i % 10 == 0:
         print "computed %d images" % i
-    if i == 50:
+    if i == NUM_IMAGES:
         break
 
     interest_points = mem.cache(get_interest_points)(mask)
--- src/compute_vocabulary.py
+++ src/compute_vocabulary.py
@@ -4,6 +4,8 @@ from load import load_data
 from descriptors import compute_boundary_desc, get_interest_points
 from vocabulary import compute_vocabulary
 
+NUM_IMAGES = 50
+
 mem = Memory(cachedir='.')
 
 gen = load_data()
@@ -12,7 +14,7 @@ print "Compute descriptors"
 for i, (im, mask) in enumerate(gen):
     if i % 10 == 0:
         print "Computed %d images" % i
-    if i == 50:
+    if i == NUM_IMAGES:
         break
     interest_points = mem.cache(get_interest_points)(mask)
     descriptor = mem.cache(compute_boundary_desc)(im, mask, interest_points)
