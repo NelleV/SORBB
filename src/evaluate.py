c502f51f60f258b5a2ba139c9267ef2e6ad77593
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2011-12-25 17:42:34 +0100

    FIX - mAP now ignores completely the images that have less than 25 % of the sculpture

--- src/evaluate.py
+++ src/evaluate.py
@@ -14,20 +14,26 @@ voc = np.load('./data/vocabulary.npy')
 postings = np.load('./data/all_postings.npy')
 all_names = np.load('./data/all_names.npy')
 
-def average_precision(search_file_names, positive_file_names, ignore_file_names):
+
+def average_precision(search_file_names, positive_file_names,
+                      ignore_file_names):
     sum_prec = 0.0
     correct = 0
+    num_doc = 0
 
     for i, search_result in enumerate(search_file_names):
-        is_true = (search_result not in ignore_file_names) and (search_result in positive_file_names) 
+        if search_result in ignore_file_names:
+            continue
+        is_true = (search_result in positive_file_names)
+        num_doc += 1
         if is_true:
             correct += 1
             sum_prec += float(correct) / (i + 1)
 
-    return sum_prec / len(search_file_names)
+    return sum_prec / num_doc
 
 
-best_prec = float("-inf")
+best_prec = - np.infty
 best_results = None
 best_query_file_name = None
 best_search_file_names = None
@@ -47,7 +53,7 @@ for test_query_index, query in enumerate(ground_truth):
 
     if query_file_name not in all_names:
         continue
-    
+
     im, mask = load.get_image(query_file_name)
     interest_points = mem.cache(get_interest_points)(mask)
     desc, coords = mem.cache(compute_boundary_desc)(im, mask, interest_points)
@@ -56,14 +62,16 @@ for test_query_index, query in enumerate(ground_truth):
         continue
 
     #search_results = retrieval.search2(visual_words, postings, max_im=20)
-    query_document, _ = np.histogram(visual_words, bins=np.arange(len(voc) + 1))
+    query_document, _ = np.histogram(visual_words,
+                                     bins=np.arange(len(voc) + 1))
     search_results = retrieval2.search(query_document, max_im=20)
     #search_results2 = mem.cache(search2)(visual_words,postings,max_im=20)
-    indices = search_results[:,0].astype(int)
+    indices = search_results[:, 0].astype(int)
     search_file_names = all_names[indices]
     queries_total += 1
-    
-    ave_prec = average_precision(search_file_names, positive_file_names, ignore_file_names)
+
+    ave_prec = average_precision(search_file_names, positive_file_names,
+                                 ignore_file_names)
     mean_average_precision += ave_prec
     print "Prec: ", ave_prec
     if ave_prec > best_prec:
@@ -71,9 +79,11 @@ for test_query_index, query in enumerate(ground_truth):
         best_search_file_names = search_file_names
         best_prec = ave_prec
         best_results = search_results
-    #retrieval.show_results(best_results, best_search_file_names, "%s (%f) mAP: %.2f" % (best_query_file_name, best_prec, mean_average_precision))
-    
 
 mean_average_precision /= queries_total
-retrieval.show_results(best_results, best_search_file_names, "%s (%f) mAP: %.2f" % (best_query_file_name, best_prec, mean_average_precision))
+retrieval.show_results(best_results,
+                       best_search_file_names,
+                       "%s (%f) mAP: %.2f" % (best_query_file_name,
+                                              best_prec,
+                                              mean_average_precision))
 pass
